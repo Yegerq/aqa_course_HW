8c5f2e334c5c5eea6919fe92bb827da69991c656
CookieFog <inffista@gmail.com>
2024-03-04 21:02:53 +0200

    Homework_3_1

--- Homework_3.py
+++ Homework_3.py
@@ -1,29 +1,24 @@
 # ******************Task#1 date 04.03.2024******************
-# Enter_word = str(input("awaiting word"))
-# new_word = Enter_word[::-1]
-# if Enter_word == new_word:
-#     print("+")
-# else:
-#     print("-")
-#
-# while Enter_word != new_word:
-#
-#     Enter_word = str(input("awaiting word"))
-#     new_word = Enter_word[::-1]
-#     if Enter_word == new_word:
-#         print("+")
-#     else:
-#         print("-")
+while True:
+    Enter_word = str(input("awaiting word"))
+    new_word = Enter_word[::-1]
+    if Enter_word == new_word:
+        print("+")
+    else:
+        print("-")
+
+
 
 # ******************Task#2 date 04.03.2024******************
-
-# text = str(input("awaiting text"))
-# word = str(input("awaiting word"))
+# while True:
 #
-# if word  not in text:
-#     print("NO")
-# else:
-#     print("YES")
+#     text = str(input("awaiting text"))
+#     word = str(input("awaiting word"))
+#
+#     if word not in text:
+#         print("NO")
+#     else:
+#         print("YES")
 
 # ******************* Task#3 date 04.03.2024*****************
 
