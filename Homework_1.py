d3ee00cfde01f2295ea3bc24989b040e929c3617
CookieFog <inffista@gmail.com>
2024-03-04 16:08:02 +0200

    Origin Main

---
+++ Homework_1.py
@@ -0,0 +1,19 @@
+MY_CONSTANT=544
+my_name = "Arsen"
+print(my_name)
+MYNAME = "Arsen"
+#Comment line 1
+#Comment line 2
+#Comment Line 3
+"""
+Comment Line 
+"""
+x = 10
+print(id(x))
+print(x)
+var1 = 5
+var2 = 6
+var3 = 7
+MY_CONSTANT = 666
+
+print(MY_CONSTANT)
